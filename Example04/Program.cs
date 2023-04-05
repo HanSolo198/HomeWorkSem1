6a7cfbe950ac85650af449ece82973596e274a4f
Denis Brykov <brykovdenis01@gmail.com>
2023-04-06 00:08:47 +0300

    added Example04

---
+++ Example04/Program.cs
@@ -0,0 +1,28 @@
+﻿//  Задача 8: Напишите программу, 
+//  которая на вход принимает число (N), 
+//  а на выходе показывает все чётные числа от 1 до N.
+
+//  5 -> 2, 4
+//  8 -> 2, 4, 6, 8
+
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.WriteLine("Введите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int c = Convert.ToInt32(1);
+
+        for (c = 1; c <= number; c++)
+        {
+            if (c % 2 == 0)
+            {
+                Console.WriteLine("Четные числа: " + c);
+            }
+
+        }
+    Console.ReadKey();
+    }
+}
