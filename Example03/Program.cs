085ef34c84b1eea8c77182ebc0e4d3b4b1f83959
Denis Brykov <brykovdenis01@gmail.com>
2023-04-05 21:28:59 +0300

    added Examle03

---
+++ Example03/Program.cs
@@ -0,0 +1,27 @@
+﻿//  Задача 6: Напишите программу,
+//которая на вход принимает число и выдаёт,
+//является ли число чётным (делится ли оно на два без остатка).
+//  4 -> да
+//  -3 -> нет
+//  7 -> нет
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.WriteLine("Введите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number % 2==1)
+        {
+                Console.WriteLine("Число нечетное");
+        }
+        else
+        {
+                Console.WriteLine("Число четное");
+        }
+
+        Console.ReadKey();
+    }
+}
